6bb86a44f6e2cbe25f3f1d42cd1101435c0fe0aa
pawan <pawandhananjay@gmail.com>
2019-09-20 12:00:28 +0530

    Send deposits at intervals sequentially

--- packages/lodestar/src/cli/commands/deposit.ts
+++ packages/lodestar/src/cli/commands/deposit.ts
@@ -2,17 +2,21 @@
  * @module cli/commands
  */
 
-import {CommanderStatic} from "commander";
-import {JsonRpcProvider} from "ethers/providers";
-import {Wallet} from "ethers/ethers";
+import { CommanderStatic } from "commander";
+import { JsonRpcProvider } from "ethers/providers";
+import { Wallet } from "ethers/ethers";
 
-import {config} from "@chainsafe/eth2.0-config/lib/presets/mainnet";
-import {CliCommand} from "./interface";
+import { config } from "@chainsafe/eth2.0-config/lib/presets/mainnet";
+import { CliCommand } from "./interface";
 import defaults from "../../eth1/options";
 import * as ethers from "ethers/ethers";
-import {ILogger, LogLevel, WinstonLogger, LogLevels} from "../../logger";
-import {Eth1Wallet} from "../../eth1";
-import {CliError} from "../error";
+import { ILogger, LogLevel, WinstonLogger, LogLevels } from "../../logger";
+import { Eth1Wallet } from "../../eth1";
+import { CliError } from "../error";
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 interface IDepositCommandOptions {
   privateKey: string;
@@ -22,6 +26,7 @@ interface IDepositCommandOptions {
   value: string;
   contract: string;
   accounts: number;
+  delay: number;
 }
 
 export class DepositCommand implements CliCommand {
@@ -29,23 +34,24 @@ export class DepositCommand implements CliCommand {
   public register(commander: CommanderStatic): void {
 
     commander
-      .command('deposit')
-      .description('Start private network with deposit contract and 10 accounts with balance')
-      .option("-k, --privateKey [privateKey]", 'Private key of account that will make deposit')
+      .command("deposit")
+      .description("Start private network with deposit contract and 10 accounts with balance")
+      .option("-k, --privateKey [privateKey]", "Private key of account that will make deposit")
       .option(`-l, --logLevel [${LogLevels.join("|")}]`, "Log level")
       .option(
         "-m, --mnemonic [mnemonic]",
-        'If mnemonic is submitted, first 10 accounts will make deposit'
+        "If mnemonic is submitted, first 10 accounts will make deposit"
       )
-      .option("-n, --node [node]", 'Url of eth1 node', 'http://127.0.0.1:8545')
-      .option("-v, --value [value]", 'Amount of ether to deposit', "32")
+      .option("-n, --node [node]", "Url of eth1 node", "http://127.0.0.1:8545")
+      .option("-v, --value [value]", "Amount of ether to deposit", "32")
       .option(
         "-c, --contract [contract]",
-        'Address of deposit contract',
+        "Address of deposit contract",
         defaults.depositContract.address
       )
-      .option("-a, --accounts [accounts]","Number of accounts to generate at startup", 10)
-      .action( async (options) => {
+      .option("-a, --accounts [accounts]", "Number of accounts to generate at startup", 10)
+      .option("-t, --delay [delay]", "Delay between sending deposits (in seconds)", 5)
+      .action(async (options) => {
         const logger: ILogger = new WinstonLogger({
           level: options.logLevel,
           module: "deposit",
@@ -55,7 +61,7 @@ export class DepositCommand implements CliCommand {
         try {
           await this.action(options, logger);
         } catch (e) {
-          console.error(e.message + '\n' + e.stack);
+          logger.error(e.message + "\n" + e.stack);
         }
       });
   }
@@ -70,32 +76,33 @@ export class DepositCommand implements CliCommand {
     }
 
     const wallets = [];
-    if(options.mnemonic) {
+    if (options.mnemonic) {
       wallets.push(...this.fromMnemonic(options.mnemonic, provider, options.accounts));
     } else if (options.privateKey) {
-      wallets.push(new Wallet(options.privateKey, provider));
+      wallets.push(new ethers.Wallet(options.privateKey, provider));
     } else {
-      throw new CliError('You have to submit either privateKey or mnemonic. Check --help');
+      throw new CliError("You have to submit either privateKey or mnemonic. Check --help");
     }
 
-    await Promise.all(
-      wallets.map(async wallet => {
-        try {
-          const hash =
-            await (new Eth1Wallet(wallet.privateKey, defaults.depositContract.abi, config, logger, provider))
-              .createValidatorDeposit(options.contract, ethers.utils.parseEther(options.value));
-          logger.info(
-            `Successfully deposited ${options.value} ETH from ${wallet.address} 
-            to deposit contract. Tx hash: ${hash}`
-          );
-        } catch (e) {
-          throw new CliError(
-            `Failed to make deposit for account ${wallet.address}. Reason: ${e.message}`
-          );
-        }
-      })
-    );
+    for (let i = 0; i < wallets.length; i++) {
+      let wallet = wallets[i];
+      try {
+        // @ts-ignore
+        const hash =
+          await (new Eth1Wallet(wallet.privateKey, defaults.depositContract.abi, config, logger, provider))
+            .createValidatorDeposit(options.contract, ethers.utils.parseEther(options.value));
+        logger.info(
+          `Successfully deposited ${options.value} ETH from ${wallet.address} 
+          to deposit contract. Tx hash: ${hash}`
+        );
+        await sleep(options.delay * 1000);
 
+      } catch (e) {
+        throw new CliError(
+          `Failed to make deposit for account ${wallet.address}. Reason: ${e.message}`
+        );
+      }
+    }
   }
 
   /**
@@ -104,10 +111,10 @@ export class DepositCommand implements CliCommand {
    * @param provider
    * @param n number of wallets to retrieve
    */
-  private fromMnemonic(mnemonic: string, provider: JsonRpcProvider, n: number): Wallet[] {
+  private fromMnemonic(mnemonic: string, provider: JsonRpcProvider, n: number): ethers.Wallet[] {
     const wallets = [];
     for (let i = 0; i < n; i++) {
-      let wallet = Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i}`);
+      let wallet = ethers.Wallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${i}`);
       wallet = wallet.connect(provider);
       wallets.push(wallet);
     }
